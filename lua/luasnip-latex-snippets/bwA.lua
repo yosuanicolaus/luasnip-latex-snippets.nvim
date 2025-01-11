c5435353e26c27feb166a0bce781154f3080a355
Yosua Nicolaus <yoni@odoo.com>
2025-01-11 18:23:32 +0100

    better template with tempd, temptma, templ

--- lua/luasnip-latex-snippets/bwA.lua
+++ lua/luasnip-latex-snippets/bwA.lua
@@ -4,6 +4,29 @@ local i = ls.insert_node
 
 local M = {}
 
+local function template_string(file_path)
+  local title_str = "\\title{${1:Reading Notes}}\n"
+  local maketitle_str = "\\maketitle\n"
+
+  if file_path == "/template/tma_natural.tex" then
+    title_str = "\\chead{${1:Reading Notes}}\n"
+    maketitle_str = ""
+  end
+
+  return "\\documentclass[a4paper,12pt]{article}\n"
+    .. "\\input{"
+    .. vim.fn.getcwd()
+    .. file_path
+    .. "}\n"
+    .. title_str
+    .. "\\date{"
+    .. os.date("%Y-%m-%d")
+    .. "}\n"
+    .. "\\begin{document}\n"
+    .. maketitle_str
+    .. "\n${2:Hello, world!}\n\n\\end{document}"
+end
+
 function M.retrieve(not_math)
   local utils = require("luasnip-latex-snippets.util.utils")
   local pipe = utils.pipe
@@ -31,6 +54,21 @@ function M.retrieve(not_math)
     ls.parser.parse_snippet(
       { trig = "cases" },
       "\\begin{cases}\n\t${1:case_1} & ${2:cond_1} \\\\\\ \n\t${3:case_2} & ${4:cond_2} \\\\\\ \n\\end{cases}"
+    ),
+
+    parse_snippet({ trig = "hr", name = "line break" }, "\\noindent\\rule{\\textwidth}{1pt}\n$1"),
+
+    parse_snippet(
+      { trig = "tempd", name = "template default.tex" },
+      template_string("/template/default.tex")
+    ),
+    parse_snippet(
+      { trig = "temptma", name = "template real TMA" },
+      template_string("/template/tma_real.tex")
+    ),
+    parse_snippet(
+      { trig = "templ", name = "template natural" },
+      template_string("/template/tma_natural.tex")
     ),
 
     s({ trig = "bigfun", name = "Big function" }, {
--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -1,15 +1,10 @@
 local ls = require("luasnip")
-local conds = require("luasnip.extras.expand_conditions")
 local utils = require("luasnip-latex-snippets.util.utils")
 local pipe = utils.pipe
 
 local M = {}
 
 function M.retrieve(not_math)
-  local ps_lb_nm = ls.extend_decorator.apply(
-    ls.parser.parse_snippet,
-    { condition = pipe({ conds.line_begin, not_math }) }
-  ) --[[@as function]]
   local parse_snippet = ls.extend_decorator.apply(ls.parser.parse_snippet, {
     condition = pipe({ not_math }),
   }) --[[@as function]]
@@ -17,19 +12,6 @@ function M.retrieve(not_math)
   return {
     parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
     parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n\\]$0"),
-
-    ps_lb_nm(
-      { trig = "templ", name = "default template" },
-      "\\documentclass[a4paper,12pt]{article}\n\\input{"
-        .. vim.fn.getcwd()
-        .. "/template/default.tex"
-        .. "}\n\\title{${1:Lecture Notes}}\n"
-        .. "\\date{"
-        .. os.date("%Y-%m-%d")
-        .. "}\n"
-        .. "\\begin{document}\n\\maketitle\n\n${2:Hello, world!}\n\n\\end{document}"
-    ),
-    ps_lb_nm({ trig = "hr", name = "line break" }, "\\noindent\\rule{\\textwidth}{1pt}\n$1"),
   }
 end
 
