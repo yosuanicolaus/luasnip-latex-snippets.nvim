1bf061c6475c0e43188ddf0ed92704c3dc852693
Yosua Nicolaus <yoni@odoo.com>
2025-01-14 23:47:25 +0100

    ps_nm . near end of this repo (will move to config)

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -5,16 +5,17 @@ local pipe = utils.pipe
 local M = {}
 
 function M.retrieve(not_math)
-  local parse_snippet = ls.extend_decorator.apply(ls.parser.parse_snippet, {
+  local ps = ls.parser.parse_snippet
+  local ps_nm = ls.extend_decorator.apply(ls.parser.parse_snippet, {
     condition = pipe({ not_math }),
   }) --[[@as function]]
 
   return {
-    parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
-    parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n\\]$0"),
+    ps_nm({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
+    ps_nm({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n\\]$0"),
 
-    parse_snippet({ trig = "txb", name = "Bold text" }, "\\textbf{$1}$0"),
-    parse_snippet({ trig = "txi", name = "Italic text" }, "\\textit{$1}$0"),
+    ps({ trig = "txb", name = "Bold text" }, "\\textbf{$1}$0"),
+    ps({ trig = "txi", name = "Italic text" }, "\\textit{$1}$0"),
   }
 end
 
